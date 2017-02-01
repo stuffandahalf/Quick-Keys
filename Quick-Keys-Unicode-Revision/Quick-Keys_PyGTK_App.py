0406f333a634c83edd6359447ed377716b63d1fb
Stuff5 <freezedown@hotmail.com>
2017-01-31 23:22:46 -0500

    added preliminary buttons

--- Quick-Keys-Unicode-Revision/Quick-Keys_PyGTK_App.py
+++ Quick-Keys-Unicode-Revision/Quick-Keys_PyGTK_App.py
@@ -17,18 +17,20 @@ import gtk
 
 k = PyKeyboard()
 
-#the port the arduino is connected to
-#ser = serial.Serial('/dev/ttyACM0')
-ser = serial.Serial('/dev/ttyUSB0')
-#ser = serial.Serial('/dev/tty.usbmodem1A21')
+window_height = 300
+window_width = 300
 
 #dictionary of symbols corresponding to the arduino
-symbols = {'1' : u'π',
-		   '2' : u'Σ',
-		   '3' : ':^)',
-		   '4' : 'sure kid',
-		   '5' : 'me too thanks',
-		   '6' : 'FeelsBindMan'}
+symbols = {'1' : 'π',
+           '2' : 'Σ',
+           '3' : 'α',
+           '4' : 'β',
+           '5' : 'Δ',
+           '6' : 'Ω'}
+
+for x in rows:
+    for y in columns:
+        coords[x+1*y+1] = 
 
 def main_script():
     while True:
@@ -67,13 +69,39 @@ def main_script():
 class Base:
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_default_size(window_width, window_height)
         self.window.show()
+        
+        layout = gtk.Layout()
+        self.window.add(layout)
+        layout.show()
+        
+        for i in symbols:                                               # for every symbol
+            entry = gtk.Entry()                                         # make a text box
+            entry.set_max_length(10)                                    # set the max length a symbol can be to 10
+            entry.set_text(symbols[str(i)])                             # set the default text in the box to the current symbol
+            layout.put(entry, 300/6*int(i)+1, 300/6*int(i)+1)           # place the text box on the layout
+            entry.show()                                                # 
     
     def main(self):
         gtk.main()
 
 if __name__ == '__main__':
-    t = threading.Thread(target = main_script)
-    t.start()
-    #main_window = Base()
-    Base().main()
+    gtk.threads_init()                                                  # initialize threads in gtk
+    t1 = threading.Thread(target = main_script)                         # create a new thread for the main script
+    
+    #try to set the port of the arduino
+    try:
+        #ser = serial.Serial('/dev/ttyACM0')
+        ser = serial.Serial('/dev/ttyUSB0')
+        #ser = serial.Serial('/dev/tty.usbmodem1A21')
+        t1.start()                                                      # start the new thread
+        print 'Serial port set to ' + ser.port
+        
+    #if none found, dont start thread
+    except:
+        ser = ''
+        print 'Serial port not found, please set later'
+    
+    main_window = Base()                                                # create a window object
+    main_window.main()                                                  # run the object
