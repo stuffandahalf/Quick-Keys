03631ec42ae9d9e1b853088d77246a62ef1b334e
stuffandahalf <freezedown@hotmail.com>
2017-02-16 02:08:15 -0500

    experimented with c++ version

--- Quick-Keys C++ Script (Linux)/Quick-Keys Script.cxx
+++ Quick-Keys C++ Script (Linux)/Quick-Keys Script.cxx
@@ -4,12 +4,13 @@
 
 using namespace std;
 
-char buffer[64];
+//char buffer[64];
 
 int main(int argc, char **argv)
 {
     int ser = open("/dev/ttyUSB0", O_RDONLY);
-    char byte[0x1000];
+    //char byte[0x1000];
+    char byte;
     while(true)
     {
         int size = read(ser, &byte, 1);
