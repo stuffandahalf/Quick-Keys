06f2dca71e4722571ed90e10eba1c5502c8945d6
stuffandahalf <freezedown@hotmail.com>
2017-03-30 14:26:21 -0400

    expanded c++ version

--- Quick-Keys C++ Script (Linux)/Quick-Keys Script.cxx
+++ Quick-Keys C++ Script (Linux)/Quick-Keys Script.cxx
@@ -1,27 +1,39 @@
 #include <iostream>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
+
+#ifdef __linux__
+    #include <X11/Xlib.h>
+    #include <X11/keysym.h>
+    #include <X11/extensions/XTest.h>
+#endif
 
 using namespace std;
 
-void main_script(char *portname);
+void main_script(const char *portname);
 
 int main(int argc, char **argv)
 {
-    main_script("/dev/ttyUSB0");
+    const char *serial_port = "/dev/ttyUSB0";
+    //pthread_create(
+    main_script(serial_port);
 	return 0;
 }
 
-void main_script(string portname)
+void main_script(const char *portname)
 {
-    portname = portname.c_str();
+    string symbols[6] = {"\u03c0", "\u03b1", "\u03a3", "\u0394", "\u03b2", "\u03a9"};
     int ser = open(portname, O_RDONLY);
     char byte[2];
     while(true)
     {
         int size = read(ser, &byte, 1);
         if(size > 0) {
-            cout << byte;
+            cout << byte[0];
+            int i = (int) byte[0];
+            cout << symbols[i];
         }
     }
 }
