2cadbe13b2ba204fd9f15dd5bcacf60aa6874ab7
Stuff5 <freezedown@hotmail.com>
2018-01-05 23:19:06 -0500

    added linux implementation of port detection

--- quick_keys_rust/src/main.rs
+++ quick_keys_rust/src/main.rs
@@ -9,8 +9,20 @@ const KEYS: usize = 6;
 
 fn main() {
     //let profiles: Vec<profile::Profile> = Vec::new();
-    let qkdev = quickkeys::QuickKeys::new();
-    //let qkdev = quickkeys::QuickKeys::new_from("COM4");
-    //let qkdev = quickkeys::QuickKeys::new_from("/dev/ttyUSB0");
+    
+    if let Ok(ports) = serialport::available_ports() {
+        for p in ports {
+            println!("{:?}", p);
+        }
+    }
+    
+    /* Initial default ports for every OS */
+    #[cfg(target_os = "linux")]
+    let qkdev = quickkeys::QuickKeys::new_on("/dev/ttyUSB0");
+    #[cfg(target_os = "windows")]
+    let qkdev = quickkeys::QuickKeys::new_on("COM4");
+    #[cfg(target_os = "macos")]
+    let qkdev = quickkeys::QuickKeys::new_on("");
+    
     qkdev.main_script();
 }
--- quick_keys_rust/src/quickkeys.rs
+++ quick_keys_rust/src/quickkeys.rs
@@ -1,5 +1,6 @@
 use serialport;
 use enigo::{Enigo, KeyboardControllable};
+use std::path::Path;
 //use std::string::String;
 
 use profile::Profile;
@@ -27,7 +28,7 @@ impl<'a> QuickKeys<'a> {
         };
     }
     
-    pub fn new_from(port_name: &'a str) -> QuickKeys<'a> {
+    pub fn new_on(port_name: &'a str) -> QuickKeys<'a> {
         return QuickKeys {
             profile: Profile::new(),
             port: port_name,
@@ -38,18 +39,16 @@ impl<'a> QuickKeys<'a> {
         let mut enigo = Enigo::new();
         let mut serial_buf: Vec<u8> = vec![0; 4];
         if let Ok(mut port) = serialport::open(self.get_port()) {
-            //while self.port_exists() {
-            //while !exit {
-            loop {
-                /*if let Ok(bytes) = port.read(serial_buf.as_mut_slice()) {
+            while self.port_exists() {
+                if let Ok(bytes) = port.read(serial_buf.as_mut_slice()) {
                     let i: usize = serial_buf[0] as usize - 49;
                     let sym = self.get_profile().get_symbol(i);
                     enigo.key_sequence(sym);
-                }*/
-                match port.read(serial_buf.as_mut_slice()) {
+                }
+                /*match port.read(serial_buf.as_mut_slice()) {
                     Ok(bytes) => println!("{}", bytes),
                     Err(err) => println!("{:?}", err),
-                }
+                }*/
             }
             println!("Device disconnected");
             
@@ -58,21 +57,32 @@ impl<'a> QuickKeys<'a> {
             println!("Error: Port '{}' not available", self.get_port());
             return 1;
         }
-        /*match serialport::open(self.get_port()) {
-            Ok(port) => println!("{:?}", port);
-            Err(err) => println!("{:?}", err);
-        }*/
         
         return 0;
     }
     
-    fn port_exists(&self) -> bool {
+    /*fn port_exists(&self) -> bool {
         if let Ok(ports) = serialport::available_ports() {
             let port_names: Vec<&str> = ports.iter().map(|s| &*s.port_name).collect();
             return port_names.contains(&self.get_port());
         }
         println!("QuickKeys on port {} disconnected", self.get_port());
         return false;
+    }*/
+    
+    #[cfg(target_os = "linux")]
+    fn port_exists(&self) -> bool {
+        return Path::new(self.get_port()).exists();
+    }
+    
+    #[cfg(target_os = "macos")]
+    fn port_exists(&self) -> bool {
+        return true;
+    }
+    
+    #[cfg(target_os = "windows")]
+    fn port_exists(&self) -> bool {
+        return true;
     }
     
     pub fn get_port(&self) -> &str {
