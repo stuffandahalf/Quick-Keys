c70c275231d5f30dfc368b19b587f8b8005810f4
Gregory Norton <freezedown@hotmail.com>
2018-02-20 22:17:25 -0800

    added core module

---
+++ quick_keys_core/src/profile.rs
@@ -0,0 +1,37 @@
+use KEYS;
+use std::string::String;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Profile {
+    name: String,
+    symbols: [String; KEYS],
+}
+
+impl Profile {
+    pub fn new() -> Profile {
+        return Profile {
+            name: String::from("Default"),
+            symbols: [String::from("π"),
+                      String::from("Σ"),
+                      String::from("α"),
+                      String::from("β"),
+                      String::from("Δ"),
+                      String::from("Ω")]
+            
+        };
+    }
+    
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+    pub fn name(&self) -> String {
+        return self.name.clone();
+    }
+    pub fn get_symbol(&self, i: usize) -> &str {
+        return &*self.symbols[i];
+    }
+    pub fn set_symbol(&mut self, index: usize, symbol: &str) {
+        self.symbols[index] = String::from(symbol);
+    }
+}
+
